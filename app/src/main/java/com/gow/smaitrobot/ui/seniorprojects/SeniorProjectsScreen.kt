2bcd496d2b9af0a157f1c739854e7b3c196fdf9d
matthewjchin <matthewjacobchin@gmail.com>
2026-05-08 14:52:49 -0700

    Merge remote-tracking branch 'origin/mjchin-alum-event' into mjchin-alum-event
    # Conflicts:
    #	app/src/main/assets/alumni_scholarship_2026_theme.json
    
    removed one of images on bottom bar of home page; also updated each of the professors' name headers in Sr Project Screen to be different from lower blue background - now dark grey instead of blue

--- app/src/main/java/com/gow/smaitrobot/ui/seniorprojects/SeniorProjectsScreen.kt
+++ app/src/main/java/com/gow/smaitrobot/ui/seniorprojects/SeniorProjectsScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -75,7 +76,7 @@ fun SeniorProjectsScreen(
                         text = "Mechanical Engineering",
                         fontSize = 44.sp,
                         fontWeight = FontWeight.ExtraBold,
-                        color = scheme.primary,
+                        color = Color.DarkGray,
                         lineHeight = 48.sp
                     )
                     Text(
